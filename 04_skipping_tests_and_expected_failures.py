c22e0cc8ed5104a44c903df95ac5e41ea1b5bb85
t3yamoto <3yamoto.dev@gmail.com>
2021-07-02 09:59:54 +0900

    04

---
+++ 04_skipping_tests_and_expected_failures.py
@@ -0,0 +1,47 @@
+import sys
+import unittest
+
+
+mylib_version = (1, 2)
+
+
+def external_resource_available():
+    return False
+
+
+class MyTestCase(unittest.TestCase):
+    @unittest.skip("demonstrating skipping")
+    def test_nothing(self):
+        self.fail("shouldn't happen")
+
+    @unittest.skipIf(mylib_version < (1, 3), "not supported in this library version")
+    def test_format(self):
+        # Tests that work for only a certain version of the library.
+        pass
+
+    @unittest.skipUnless(sys.platform.startswith("win"), "requires Windows")
+    def test_windows_support(self):
+        # windows specific testing code
+        pass
+
+    def test_maybe_skipped(self):
+        if not external_resource_available():
+            self.skipTest("external resource not available")
+        # test code that depends on the external resource
+        pass
+
+
+@unittest.skip("showing class skipping")
+class MySkippedTestCase(unittest.TestCase):
+    def test_not_run(self):
+        pass
+
+
+class ExpectedFailureTestCase(unittest.TestCase):
+    @unittest.expectedFailure
+    def test_fail(self):
+        self.assertEqual(1, 0, "broken")
+
+
+if __name__ == "__main__":
+    unittest.main()
